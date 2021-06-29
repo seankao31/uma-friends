77018cd99564ee5591b62de96dfcbb2691322aeb
Sean Kao <seankao31@gmail.com>
2021-06-30 00:25:32 +0800

    scrape friends from gamewith

---
+++ uma_friends/parse_and_write.py
@@ -0,0 +1,115 @@
+from bs4 import BeautifulSoup
+from pymongo import MongoClient, DESCENDING
+from pymongo.errors import DuplicateKeyError
+
+
+db_name = 'raw_gamewith_uma_friends'
+collection_name = 'raw_friends'
+
+
+# Print iterations progress
+def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd='\r'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
+def parse_and_write(tmp='scraped'):
+    with MongoClient() as mongo_client:
+        db = mongo_client[db_name]
+        raw_friends = db[collection_name]
+        raw_friends.create_index(
+            [('friend_code', DESCENDING), ('post_date', DESCENDING)],
+            unique=True
+        )
+
+        with open(tmp, 'r', encoding='utf8') as f:
+            print(f'Reading file: {tmp}')
+            page_html = f.read()
+            print('Parsing file...')
+            page = BeautifulSoup(page_html, features="html.parser")
+
+            friends = list(page.ul)
+            # Insert in the order they're uploaded to the website
+            friends.reverse()
+            n = len(friends)
+
+            print(f'Inserting into database {db_name}/{collection_name}')
+            if n != 0:
+                print_progress_bar(0, n, prefix='Progress:', suffix='Complete', length=50)
+            duplicate_count = 0
+            for i, friend in enumerate(friends):
+                support_id = None
+                support_wrap = friend.find_all(class_='-r-uma-musume-friends-list-item__support-wrap')
+                if support_wrap:
+                    support_id = support_wrap[0].find_all('a')[0].get('href')
+                    support_id = support_id.split('/')[-1]
+
+                support_limit = None
+                support_limit_wrap = friend.find_all(class_='-r-uma-musume-friends-list-item__limitNumber')
+                if support_limit_wrap:
+                    support_limit = support_limit_wrap[0].text.strip()
+
+                trainer_id = None
+                trainer_id_wrap = friend.find_all(class_='-r-uma-musume-friends-list-item__trainerId__text')
+                if trainer_id_wrap:
+                    trainer_id = trainer_id_wrap[0].text.strip()
+
+                main_uma_img = None
+                main_uma_wrap = friend.find_all(class_='-r-uma-musume-friends-list-item__mainUmaMusume-wrap')
+                if main_uma_wrap:
+                    main_uma_img = main_uma_wrap[0].img.get('src').strip()
+
+                factors = None
+                factors_item = friend.find_all(class_='-r-uma-musume-friends-list-item__factor-list__item')
+                if factors_item:
+                    factors = [factor.text.strip() for factor in factors_item]
+
+                comment = None
+                comment_wrap = friend.find_all(class_='-r-uma-musume-friends-list-item__comment')
+                if comment_wrap:
+                    comment = comment_wrap[0].text.strip()
+
+                post_date = None
+                post_date_wrap = friend.find_all(class_='-r-uma-musume-friends-list-item__postDate')
+                if post_date_wrap:
+                    post_date = post_date_wrap[0].text.strip()
+
+                entry = {
+                    'friend_code': trainer_id,
+                    'support_id': support_id,
+                    'support_limit': support_limit,
+                    'character_image_url': main_uma_img,
+                    'factors': factors,
+                    'comment': comment,
+                    'post_date': post_date
+                }
+                print_progress_bar(i+1, n, prefix='Progress:', suffix='Complete', length=50)
+                try:
+                    raw_friends.insert_one(entry)
+                except DuplicateKeyError:
+                    duplicate_count += 1
+                    continue
+            print('Finished!')
+            num_new_records = n - duplicate_count
+            print(f'New records: {num_new_records}')
+
+
+if __name__ == '__main__':
+    parse_and_write()
---
+++ uma_friends/scrape_raw.py
@@ -0,0 +1,124 @@
+import time
+from pymongo import MongoClient
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+url = "https://gamewith.jp/uma-musume/article/show/260740"
+db_name = 'raw_gamewith_uma_friends'
+collection_name = 'raw_friends'
+BUTTON_LIMIT = 200
+
+
+def get_friends_list(friends_section):
+    friends = []
+    for _ in range(15):
+        if friends:
+            break
+        print('Getting friends list...')
+        friends = friends_section.find_elements_by_class_name('-r-uma-musume-friends-list-item')
+        time.sleep(2)
+    return friends
+
+
+def scrape(tmp='scraped'):
+    with MongoClient() as mongo_client:
+        db = mongo_client[db_name]
+        raw_friends = db[collection_name]
+
+        try:
+            chrome_op = webdriver.ChromeOptions()
+            chrome_op.add_argument('headless')
+            driver = webdriver.Chrome(options=chrome_op)
+            print(f'Connecting to {url} ...')
+            driver.get(url)
+
+            # Get to the friends section
+            friends_section = driver.find_element_by_tag_name('gds-uma-musume-friends')
+            friends_section = driver.execute_script('return arguments[0].shadowRoot', friends_section)
+            print('Waiting for page load...')
+            # Ensure shadowRoot executed by finding elements inside it
+            while True:
+                if not friends_section.find_elements_by_class_name('-r-uma-musume-friends__search-wrap'):
+                    time.sleep(0.5)
+                else:
+                    break
+
+            # Repeatly click "もっと見る" button until conditions met.
+            # Each time the page loads 200 more records.
+            button_click_count = 0
+            while True:
+                # 1) Limit reached
+                # Page crashes if it's too huge (~40000 records i.e. 200 clicks)
+                if button_click_count == BUTTON_LIMIT:
+                    break
+
+                # If local MongoDB isn't empty
+                if raw_friends.count_documents({}) != 0:
+                    friends = get_friends_list(friends_section)
+                    print(f'Fetched {len(friends)} friends...')
+                    if not friends:
+                        break
+
+                    # Check if the last record on page is already in db.
+                    last_friend = friends[-1]
+                    trainer_id = (last_friend
+                                  .find_element_by_class_name('-r-uma-musume-friends-list-item__trainerId__text')
+                                  .text)
+                    post_date = (last_friend
+                                 .find_element_by_class_name('-r-uma-musume-friends-list-item__postDate')
+                                 .text)
+                    # 2) Record already in db
+                    if raw_friends.count_documents({'friend_code': trainer_id, 'post_date': post_date}) != 0:
+                        print(f'Record already in database: ({trainer_id}, {post_date})')
+                        print('Stop clicking.')
+                        break
+
+                # Try to click "もっと見る" button
+                button_clicked = False
+                try_more_button_cnt = 0
+                while try_more_button_cnt <= 10:
+                    try:
+                        print('Finding もっと見る button...')
+                        more_friends_button = friends_section.find_element_by_class_name('-r-uma-musume-friends__next')
+                    except NoSuchElementException:
+                        try_more_button_cnt += 1
+                        time.sleep(1)
+                    else:
+                        # more_friends_button.click()  # won't work
+                        # ElementClickInterceptedException: is not clickable at point (450, 515). Other element would receive the click
+                        driver.execute_script("arguments[0].click();", more_friends_button)
+                        print('もっと見る button clicked.')
+                        button_clicked = True
+                        button_click_count += 1
+                        # Avoid rapid clicking
+                        time.sleep(2)
+                        break
+
+                # 3) Can't find "もっと見る" button
+                if not button_clicked:
+                    break
+
+            print('Waiting for page load...')
+            time.sleep(10)
+            # Says how many records searched and how many of them are shown on page.
+            # By default the 2 figures should match, because we are searching for ALL records,
+            # rather than querying under some conditions.
+            # If the "もっと見る" button is pressed too fast, new records are searched but won't
+            # be loaded to the page.
+            print(friends_section.find_element_by_class_name('-r-uma-musume-friends__results').text)
+
+            print(f'Writing to file: {tmp}')
+            with open(tmp, 'w', encoding='utf8') as f:
+                f.write(friends_section.get_attribute('innerHTML'))
+
+        except Exception as e:
+            # To avoid premature exits leaving zombie processes
+            driver.quit()
+            raise e
+
+        driver.quit()
+
+
+if __name__ == '__main__':
+    scrape()
