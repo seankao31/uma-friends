a445a2605f29b9e83342f57c4aabefb372918038
Sean Kao <seankao31@gmail.com>
2021-07-05 21:44:30 +0800

    refactor: abstract into functions

--- uma_friends/scrape_friends_gamewith.py
+++ uma_friends/scrape_friends_gamewith.py
@@ -8,18 +8,25 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 
-uma_musume_game_db = 'uma_musume_game'
-raw_gamewith_uma_friends_db = 'raw_gamewith_uma_friends'
-raw_friends_collection = 'raw_friends'
-uma_friends_db = 'uma_friends'
-uma_friends_collection = 'friends'
+# game data
+UMA_MUSUME_GAME_DB = 'uma_musume_game'
+
+# friend raw data scraped from gamewith
+RAW_GAMEWITH_UMA_FRIENDS_DB = 'raw_gamewith_uma_friends'
+RAW_FRIENDS_COLLECTION = 'raw_friends'
+
+# cleaned up friend data
+UMA_FRIENDS_DB = 'uma_friends'
+UMA_FRIENDS_COLLECTION = 'friends'
 
 
-gamewith_friends_url = "https://gamewith.jp/uma-musume/article/show/260740"
+GAMEWITH_FRIENDS_URL = "https://gamewith.jp/uma-musume/article/show/260740"
 BUTTON_LIMIT = 200
 
 
 def get_friends_list(friends_section):
+    '''Returns the list of friends presented in friends_section.'''
+
     friends = []
     for _ in range(15):
         if friends:
@@ -30,10 +37,63 @@ def get_friends_list(friends_section):
     return friends
 
 
+def get_friends_section(driver):
+    '''Finds friends_section in page and execute shadowRoot. Returns friends_section.'''
+
+    friends_section = driver.find_element_by_tag_name('gds-uma-musume-friends')
+    friends_section = driver.execute_script('return arguments[0].shadowRoot', friends_section)
+    print('Waiting for page load...')
+    # Ensure shadowRoot executed by finding elements inside it
+    while True:
+        if not friends_section.find_elements_by_class_name('-r-uma-musume-friends__search-wrap'):
+            time.sleep(0.5)
+        else:
+            break
+    return friends_section
+
+
+def is_friend_in_db(friend, collection):
+    '''Returns whether friend is already in collection.'''
+
+    trainer_id = (friend
+                  .find_element_by_class_name('-r-uma-musume-friends-list-item__trainerId__text')
+                  .text)
+    post_date = (friend
+                 .find_element_by_class_name('-r-uma-musume-friends-list-item__postDate')
+                 .text)
+    if collection.count_documents({'friend_code': trainer_id, 'post_date': post_date}) != 0:
+        print(f'Record already in database: ({trainer_id}, {post_date})')
+        return True
+
+    return False
+
+
+def click_more_friends_button(driver, friends_section):
+    '''Attempts to click "もっと見る" button. Returns whether it's successful.'''
+
+    try_more_button_cnt = 0
+    while try_more_button_cnt <= 10:
+        try:
+            print('Finding もっと見る button...')
+            more_friends_button = friends_section.find_element_by_class_name('-r-uma-musume-friends__next')
+        except NoSuchElementException:
+            try_more_button_cnt += 1
+            time.sleep(1)
+        else:
+            # more_friends_button.click()  # won't work
+            # above will throw ElementClickInterceptedException
+            driver.execute_script("arguments[0].click();", more_friends_button)
+            print('もっと見る button clicked.')
+            return True
+    return False
+
+
 def scrape_raw(url):
+    '''Scrapes url and returns raw friend list html.'''
+
     with MongoClient() as mongo_client:
-        db = mongo_client[raw_gamewith_uma_friends_db]
-        raw_friends = db[raw_friends_collection]
+        db = mongo_client[RAW_GAMEWITH_UMA_FRIENDS_DB]
+        raw_friends = db[RAW_FRIENDS_COLLECTION]
 
         try:
             chrome_op = webdriver.ChromeOptions()
@@ -42,16 +102,8 @@ def scrape_raw(url):
             print(f'Connecting to {url} ...')
             driver.get(url)
 
-            # Get to the friends section
-            friends_section = driver.find_element_by_tag_name('gds-uma-musume-friends')
-            friends_section = driver.execute_script('return arguments[0].shadowRoot', friends_section)
-            print('Waiting for page load...')
-            # Ensure shadowRoot executed by finding elements inside it
-            while True:
-                if not friends_section.find_elements_by_class_name('-r-uma-musume-friends__search-wrap'):
-                    time.sleep(0.5)
-                else:
-                    break
+            # Get the friends section on the page
+            friends_section = get_friends_section(driver)
 
             # Repeatly click "もっと見る" button until conditions met.
             # Each time the page loads 200 more records.
@@ -62,55 +114,33 @@ def scrape_raw(url):
                 if button_click_count == BUTTON_LIMIT:
                     break
 
-                # If local MongoDB isn't empty
+                # If db isn't empty
                 if raw_friends.count_documents({}) != 0:
                     friends = get_friends_list(friends_section)
                     print(f'Fetched {len(friends)} friends...')
                     if not friends:
                         break
 
-                    # Check if the last record on page is already in db.
+                    # 2) Last (oldest) record on current page is already in db
                     last_friend = friends[-1]
-                    trainer_id = (last_friend
-                                  .find_element_by_class_name('-r-uma-musume-friends-list-item__trainerId__text')
-                                  .text)
-                    post_date = (last_friend
-                                 .find_element_by_class_name('-r-uma-musume-friends-list-item__postDate')
-                                 .text)
-                    # 2) Record already in db
-                    if raw_friends.count_documents({'friend_code': trainer_id, 'post_date': post_date}) != 0:
-                        print(f'Record already in database: ({trainer_id}, {post_date})')
+                    if is_friend_in_db(friend=last_friend, collection=raw_friends):
                         print('Stop clicking.')
                         break
 
                 # Try to click "もっと見る" button
-                button_clicked = False
-                try_more_button_cnt = 0
-                while try_more_button_cnt <= 10:
-                    try:
-                        print('Finding もっと見る button...')
-                        more_friends_button = friends_section.find_element_by_class_name('-r-uma-musume-friends__next')
-                    except NoSuchElementException:
-                        try_more_button_cnt += 1
-                        time.sleep(1)
-                    else:
-                        # more_friends_button.click()  # won't work
-                        # ElementClickInterceptedException: is not clickable at point (450, 515). Other element would receive the click
-                        driver.execute_script("arguments[0].click();", more_friends_button)
-                        print('もっと見る button clicked.')
-                        button_clicked = True
-                        button_click_count += 1
-                        # Avoid rapid clicking
-                        time.sleep(2)
-                        break
-
-                # 3) Can't find "もっと見る" button
-                if not button_clicked:
+                is_button_clicked = click_more_friends_button(driver, friends_section)
+                if is_button_clicked:
+                    button_click_count += 1
+                    # Avoid rapid clicking
+                    time.sleep(2)
+                else:
+                    # 3) Can't find "もっと見る" button
                     break
 
             print('Waiting for page load...')
             time.sleep(10)
-            # Says how many records searched and how many of them are shown on page.
+
+            # Prints out how many records are searched and how many of them are shown on page.
             # By default the 2 figures should match, because we are searching for ALL records,
             # rather than querying under some conditions.
             # If the "もっと見る" button is pressed too fast, new records are searched but won't
@@ -122,6 +152,7 @@ def scrape_raw(url):
         except Exception as e:
             # To avoid premature exits leaving zombie processes
             driver.quit()
+            # We don't really deal with the exception though, hence raising it
             raise e
 
         driver.quit()
@@ -130,16 +161,20 @@ def scrape_raw(url):
 
 
 def parse_page(page_html):
+    '''Parses page_html. Returns list of ul elements each containing data of one friend.'''
+
     print('Parsing page html...')
     page = BeautifulSoup(page_html, 'lxml')
 
     friends = list(page.ul)
-    # To insert in the order they're uploaded to the website
+    # Reverse to insert in the order they're uploaded to the website
     friends.reverse()
     return friends
 
 
 def get_friends(friends_page_list):
+    '''Generator that yields entries. Each entry is a dict that contains friend information we need.'''
+
     progress_bar = IncrementalBar('Processing', max=len(friends_page_list))
     for friend in progress_bar.iter(friends_page_list):
         support_id = None
@@ -192,20 +227,20 @@ def get_friends(friends_page_list):
 
 
 if __name__ == '__main__':
-    raw_page = scrape_raw(url=gamewith_friends_url)
+    raw_page = scrape_raw(url=GAMEWITH_FRIENDS_URL)
     parsed_list = parse_page(page_html=raw_page)
     n_friends = len(parsed_list)
     friends = get_friends(friends_page_list=parsed_list)
 
     mongo_client = MongoClient()
-    raw_db = mongo_client[raw_gamewith_uma_friends_db]
-    raw_friends = raw_db[raw_friends_collection]
+    raw_db = mongo_client[RAW_GAMEWITH_UMA_FRIENDS_DB]
+    raw_friends = raw_db[RAW_FRIENDS_COLLECTION]
     raw_friends.create_index(
         [('friend_code', DESCENDING), ('post_date', DESCENDING)],
         unique=True
     )
 
-    print(f'Inserting into database {raw_gamewith_uma_friends_db}/{raw_friends_collection}')
+    print(f'Inserting into database {RAW_GAMEWITH_UMA_FRIENDS_DB}/{RAW_FRIENDS_COLLECTION}')
     duplicate_count = 0
     for friend in friends:
         try:
