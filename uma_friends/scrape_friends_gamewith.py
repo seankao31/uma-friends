659ca1c87691d7787c1f79953046f032593200ae
Sean Kao <seankao31@gmail.com>
2021-07-09 02:41:18 +0800

    atlas, logging, timeout for page, PageError

--- uma_friends/scrape_friends_gamewith.py
+++ uma_friends/scrape_friends_gamewith.py
@@ -1,21 +1,39 @@
 import configparser
+import json
+import logging
 import os
 import time
 
 from bs4 import BeautifulSoup
 from pymongo import DESCENDING, MongoClient
-from pymongo.errors import BulkWriteError, DuplicateKeyError
+from pymongo.errors import BulkWriteError
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 
 env = 'TEST'
-# env = 'PROD
+# PROD or TEST
 config = configparser.ConfigParser()
 config.read(os.path.abspath(os.path.join(".ini")))
 
+
+def get_logger():
+    logger = logging.getLogger('scrape_friends_gamewith')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+logger = get_logger()
+
 # Sadly it seems that there's no other way other than using magic number
 DUPLICATE_KEY_ERROR_CODE = 11000
+BUTTON_LIMIT = config[env]['BUTTON_LIMIT']
 
 # game data
 UMA_MUSUME_GAME_DB = 'uma_musume_game'
@@ -30,8 +48,15 @@ UMA_FRIENDS_DB = 'uma_friends'
 UMA_FRIENDS_COLLECTION = 'friends'
 
 
-GAMEWITH_FRIENDS_URL = "https://gamewith.jp/uma-musume/article/show/260740"
-BUTTON_LIMIT = 200
+GAMEWITH_FRIENDS_URL = config['URL']['GAMEWITH_FRIENDS_URL']
+
+
+class PageError(Exception):
+    pass
+
+
+def message_with_json(msg, json_object):
+    return msg + ' ' + json.dumps(json_object, ensure_ascii=False)
 
 
 def get_friends_list(friends_section):
@@ -41,24 +66,28 @@ def get_friends_list(friends_section):
     for _ in range(15):
         if friends:
             break
-        print('Getting friends list...')
         friends = friends_section.find_elements_by_class_name('-r-uma-musume-friends-list-item')
         time.sleep(2)
+    logger.info('Got friend list on page.')
     return friends
 
 
-def get_friends_section(driver):
+def get_friends_section(driver, timeout=30):
     '''Finds friends_section in page and execute shadowRoot. Returns friends_section.'''
 
     friends_section = driver.find_element_by_tag_name('gds-uma-musume-friends')
     friends_section = driver.execute_script('return arguments[0].shadowRoot', friends_section)
-    print('Waiting for page load...')
     # Ensure shadowRoot executed by finding elements inside it
-    while True:
+    timeout_counter = 0
+    while timeout_counter < timeout:
         if not friends_section.find_elements_by_class_name('-r-uma-musume-friends__search-wrap'):
-            time.sleep(0.5)
+            timeout_counter += 1
+            time.sleep(1)
         else:
             break
+    if timeout_counter == timeout:
+        raise PageError('Cannot execute page shadowroot.')
+    logger.info('Loaded page shadowroot.')
     return friends_section
 
 
@@ -72,33 +101,33 @@ def is_friend_in_db(friend, collection):
                  .find_element_by_class_name('-r-uma-musume-friends-list-item__postDate')
                  .text)
     if collection.count_documents({'friend_code': trainer_id, 'post_date': post_date}) != 0:
-        print(f'Record already in database: ({trainer_id}, {post_date})')
+        message = message_with_json('Found duplicate record on page.',
+                                    {'friend_code': trainer_id, 'post_date': post_date})
+        logger.info(message)
         return True
 
     return False
 
 
-def click_more_friends_button(driver, friends_section):
+def click_more_friends_button(driver, friends_section, timeout=20):
     '''Attempts to click "もっと見る" button. Returns whether it's successful.'''
 
-    try_more_button_cnt = 0
-    while try_more_button_cnt <= 10:
+    timeout_counter = 0
+    while timeout_counter <= timeout:
         try:
-            print('Finding もっと見る button...')
             more_friends_button = friends_section.find_element_by_class_name('-r-uma-musume-friends__next')
         except NoSuchElementException:
-            try_more_button_cnt += 1
+            timeout_counter += 1
             time.sleep(1)
         else:
             # more_friends_button.click()  # won't work
             # above will throw ElementClickInterceptedException
             driver.execute_script("arguments[0].click();", more_friends_button)
-            print('もっと見る button clicked.')
             return True
     return False
 
 
-def scrape_raw(url):
+def scrape_raw(url, timeout=100):
     '''Scrapes url and returns raw friend list html.'''
 
     with MongoClient(UMAFRIENDS_DB_URI) as mongo_client:
@@ -109,8 +138,8 @@ def scrape_raw(url):
             chrome_op = webdriver.ChromeOptions()
             chrome_op.add_argument('headless')
             driver = webdriver.Chrome(options=chrome_op)
-            print(f'Connecting to {url} ...')
             driver.get(url)
+            logger.info(message_with_json('Connected to page.', {'url': url}))
 
             # Get the friends section on the page
             friends_section = get_friends_section(driver)
@@ -122,46 +151,59 @@ def scrape_raw(url):
                 # 1) Limit reached
                 # Page crashes if it's too huge (~40000 records i.e. 200 clicks)
                 if button_click_count == BUTTON_LIMIT:
+                    logger.info(message_with_json('Reached button click limit.', {'limit': BUTTON_LIMIT}))
                     break
 
                 # If db isn't empty
                 if raw_friends.count_documents({}) != 0:
                     friends = get_friends_list(friends_section)
-                    print(f'Fetched {len(friends)} friends...')
+                    logger.info(message_with_json('Fetched friends.', {'count': len(friends)}))
                     if not friends:
+                        logger.warning(message_with_json('There are no friends on page.', {'url': url}))
                         break
 
                     # 2) Last (oldest) record on current page is already in db
                     last_friend = friends[-1]
                     if is_friend_in_db(friend=last_friend, collection=raw_friends):
-                        print('Stop clicking.')
                         break
 
                 # Try to click "もっと見る" button
                 is_button_clicked = click_more_friends_button(driver, friends_section)
                 if is_button_clicked:
                     button_click_count += 1
+                    logger.info('Clicked button. {\'button\': もっと見る}')
                     # Avoid rapid clicking
                     time.sleep(2)
                 else:
                     # 3) Can't find "もっと見る" button
+                    logger.info('Button not found. {\'button\': もっと見る}')
                     break
 
-            print('Waiting for page load...')
-            time.sleep(10)
+            logger.info('Stopped clicking button. {\'button\': もっと見る}')
 
-            # Prints out how many records are searched and how many of them are shown on page.
-            # By default the 2 figures should match, because we are searching for ALL records,
-            # rather than querying under some conditions.
-            # If the "もっと見る" button is pressed too fast, new records are searched but won't
-            # be loaded to the page.
-            print(friends_section.find_element_by_class_name('-r-uma-musume-friends__results').text)
+            timeout_counter = 0
+            while timeout_counter < timeout:
+                page_result = friends_section.find_elements_by_class_name('-r-uma-musume-friends__results')
+                if not page_result:
+                    timeout_counter += 1
+                    time.sleep(1)
+                else:
+                    # Logs how many records are searched and how many of them are shown on page.
+                    # By default the 2 figures should match, because we are searching for ALL records,
+                    # rather than querying under some conditions.
+                    # If the "もっと見る" button is pressed too frequently, new records are searched but won't
+                    # be loaded to the page.
+                    logger.info(message_with_json('Friend section loaded.', {'result': page_result[0].text}))
+                    break
+            if timeout_counter == timeout:
+                raise PageError('Cannot load friend section.')
 
             page_content = friends_section.get_attribute('innerHTML')
 
         except Exception as e:
             # To avoid premature exits leaving zombie processes
             driver.quit()
+            logger.exception()
             # We don't really deal with the exception though, hence raising it
             raise e
 
@@ -173,8 +215,8 @@ def scrape_raw(url):
 def parse_page(page_html):
     '''Parses page_html. Returns list of ul elements each containing data of one friend.'''
 
-    print('Parsing page html...')
     page = BeautifulSoup(page_html, 'lxml')
+    logger.info('Parsed html.')
 
     friends = list(page.ul)
     return friends
@@ -239,8 +281,8 @@ def get_friends(friends_page_list):
 if __name__ == '__main__':
     raw_page = scrape_raw(url=GAMEWITH_FRIENDS_URL)
     parsed_list = parse_page(page_html=raw_page)
-    n_friends = len(parsed_list)
     friends = get_friends(friends_page_list=parsed_list)
+    logger.info(message_with_json('Fetched friends on page to insert.', {'count': len(friends)}))
 
     mongo_client = MongoClient(UMAFRIENDS_DB_URI)
     raw_db = mongo_client[UMAFRIENDS_DB]
@@ -250,19 +292,20 @@ if __name__ == '__main__':
         unique=True
     )
 
-    print(f'Inserting into database {UMAFRIENDS_DB}/{RAW_GAMEWITH_FRIENDS_NS}')
-    friends_to_insert = []
-    for friend in friends:
-        friends_to_insert.append(friend)
-
+    message = message_with_json('Insert into database.',
+                                {'uri': UMAFRIENDS_DB_URI, 'database': UMAFRIENDS_DB, 'collection': RAW_GAMEWITH_FRIENDS_NS})
+    logger.info(message)
     try:
-        insert_result = raw_friends.insert_many(friends_to_insert, ordered=False)
+        insert_result = raw_friends.insert_many(friends, ordered=False)
     except BulkWriteError as e:
-        panic_list = list(filter(lambda x: x['code'] != DUPLICATE_KEY_ERROR_CODE,
-                                 e.details['writeErrors']))
+        n_error = len(e.details['writeErrors'])
+        panic_list = [err for err in e.details['writeErrors'] if err['code'] != DUPLICATE_KEY_ERROR_CODE]
+        n_duplicate_key_error = n_error - len(panic_list)
+        logger.debug(message_with_json('Ignored duplicate key error.', {'count': n_duplicate_key_error}))
         e.details['writeErrors'] = panic_list
-        print(f'New records: {e.details["nInserted"]}')
+        logger.info(message_with_json('Insert finished.', {'nInserted': e.details["nInserted"]}))
         if panic_list:
+            logger.exception()
             raise e
     else:
-        print(f'New records: {insert_result["nInserted"]}')
+        logger.info(message_with_json('Insert finished.', {'nInserted': len(insert_result.inserted_ids)}))
